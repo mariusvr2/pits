6acfbc7651678fea04de54a962fb6c2f774dc3ee
GGPC <GGPC@DESKTOP-JJ68KEB>
2019-11-06 09:58:12 +1300

    debug system and gps time

--- tracker/lora.c
+++ tracker/lora.c
@@ -602,7 +602,7 @@ int UplinkTimeToSendOnThisChannel(int LoRaChannel, struct TGPS *GPS)
 		long CycleSeconds;
 		
 		CycleSeconds = GPS->SecondsInDay % Config.LoRaDevices[LoRaChannel].UplinkCycle;
-		printf ("Time: %ld\n", CycleSeconds);
+		printf ("system time: %s GPS Time: %ld\n", time(),CycleSeconds);
 		if (CycleSeconds < Config.LoRaDevices[LoRaChannel].UplinkPeriod)
 		{
 			return 0;
